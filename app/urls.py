68f067acc21b0252908a9aef2597dcdb41f1ae00
mouhazba <bamahadiou@gmail.com>
2022-11-10 23:28:12 +0000

    crud books

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPage
+from .views import HomePageView, AboutPage, BookListView, BookDetailView
 
 
 urlpatterns = [
 path('', HomePageView.as_view(), name='home'),
 path('about/', AboutPage.as_view(), name='about'),
+path('books/', BookListView.as_view(), name='book_list'),
+path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,24 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+
+from .models import Book
+
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 
-
 class AboutPage(TemplateView):
     template_name = 'about.html'
+
+
+class BookListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'app/book_list.html'
+
+
+class BookDetailView(DetailView):
+    model = Book
+    context_object_name = 'book'
+    template_name = 'app/book_detail.html'
